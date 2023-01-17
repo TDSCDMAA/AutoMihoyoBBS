3d5e01d16ba177e51234d80c79ed3957a2515c68
大山 <10679024+qizitong20100501@user.noreply.gitee.com>
2023-01-17 12:04:49 +0000

    update captcha.py.
    Signed-off-by: 大山 <10679024+qizitong20100501@user.noreply.gitee.com>

--- captcha.py
+++ captcha.py
@@ -1,9 +1,15 @@
-from request import http
+from geetestServer import GeetestValidateServer
+from loghelper import log
 
 
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
+     #启动网页服务器，要求用户完成米哈游的验证码
+    log.info("遭遇验证码，请在浏览器中打开 http://127.0.0.1:16384 来完成验证码")
+    g = GeetestValidateServer()
+    return g.solveCaptcha(challenge,gt)  # 失败返回None 成功返回validate
 
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+    log.info("遭遇验证码，请在浏览器中打开 http://127.0.0.1:16384 来完成验证码")
+    g = GeetestValidateServer()
+    return g.solveCaptcha(challenge,gt)
